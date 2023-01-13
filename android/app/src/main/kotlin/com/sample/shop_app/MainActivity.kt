f2d305ffaad92ae784e5c85af34a47f64656648b
Alin Babu <alinbabu2010@gmail.com>
2023-01-13 11:54:58 +0530

    Initial commit with project setup

---
+++ android/app/src/main/kotlin/com/sample/shop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sample.shop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShopApp());
+}
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ShopHomePage(),
+    );
+  }
+}
+
+class ShopHomePage extends StatelessWidget {
+  const ShopHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+      ),
+      body: const Center(child: Text("Let's build a shop!")),
+    );
+  }
+}
