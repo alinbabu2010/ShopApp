c4cd498cc075b8f5f64995a505c532021d79e18c
Alin Babu <alinbabu2010@gmail.com>
2023-01-13 15:29:00 +0530

    Add products overview screen

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/widgets/product_item.dart';
+
+import '../models/product.dart';
+import '../utils/dimens.dart';
+
+class ProductsOverview extends StatelessWidget {
+  final List<Product> products = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
+    Product(
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
+  ];
+
+  ProductsOverview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MyShop"),
+      ),
+      body: GridView.builder(
+        gridDelegate: productOverviewGridDelegate,
+        padding: productOverviewGridPadding,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return ProductItem(
+            id: product.id,
+            title: product.title,
+            imageUrl: product.imageUrl,
+          );
+        },
+        itemCount: products.length,
+      ),
+    );
+  }
+}
