f5feefdf29bcb5dfd81fc26a8e6699a0be65a636
Alin Babu <alinbabu2010@gmail.com>
2023-01-16 12:11:39 +0530

    Add variable to get product from provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product_detail_screen';
@@ -7,10 +9,11 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var id = ModalRoute.of(context)?.settings.arguments as String;
+    final id = ModalRoute.of(context)?.settings.arguments as String;
+    final product = Provider.of<Products>(context, listen: false).findById(id);
     return Scaffold(
       appBar: AppBar(
-        title: Text(id),
+        title: Text(product.title),
       ),
     );
   }
