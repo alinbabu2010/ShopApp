be942b6c36c921efe74e0204b6f483af34ef058d
Alin Babu <alinbabu2010@gmail.com>
2023-01-16 11:57:38 +0530

    Add changeNotifierProvider widget to ShopApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:shop_app/utils/constants.dart';
@@ -12,19 +14,22 @@ class ShopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appName,
-      theme: ThemeData(
-          colorScheme: const ColorScheme.light(
-            primary: Colors.indigo,
-            secondary: Colors.deepOrange,
-          ),
-          fontFamily: latoFont),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        ProductsOverview.routeName: (context) => ProductsOverview(),
-        ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
-      },
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: appName,
+        theme: ThemeData(
+            colorScheme: const ColorScheme.light(
+              primary: Colors.indigo,
+              secondary: Colors.deepOrange,
+            ),
+            fontFamily: latoFont),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          ProductsOverview.routeName: (context) => ProductsOverview(),
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
