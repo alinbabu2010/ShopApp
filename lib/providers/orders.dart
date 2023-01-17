ac366d502f08893facf2bfb243b7312325429355
Alin Babu <alinbabu2010@gmail.com>
2023-01-17 12:01:01 +0530

    Add orders class and provider

---
+++ lib/providers/orders.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shop_app/models/cart_item.dart';
+import 'package:shop_app/models/order_item.dart';
+
+class Orders with ChangeNotifier {
+  final List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+
+  void addOrder(List<CartItem> cartProducts, double total) {
+    _orders.insert(
+        0,
+        OrderItem(
+          DateTime.now().toString(),
+          total,
+          cartProducts,
+          DateTime.now(),
+        ));
+    notifyListeners();
+  }
+}
