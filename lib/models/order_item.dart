5e587a55dc5917129b62e39199108b3f7d43169b
Alin Babu <alinbabu2010@gmail.com>
2023-01-17 12:00:09 +0530

    Add order item data class

---
+++ lib/models/order_item.dart
@@ -0,0 +1,10 @@
+import 'package:shop_app/models/cart_item.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem(this.id, this.amount, this.products, this.dateTime);
+}
