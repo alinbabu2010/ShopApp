3a04549b90ed7c376fcf6b569740b409a45e9236
Alin Babu <alinbabu2010@gmail.com>
2023-01-17 09:49:29 +0530

    Add function to get totalAmount in cart.dart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -23,6 +23,14 @@ class Cart with ChangeNotifier {
 
   int get itemCount => _items.length;
 
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += (cartItem.price * cartItem.quantity);
+    });
+    return total;
+  }
+
   void addItem(
     String productId,
     double price,
