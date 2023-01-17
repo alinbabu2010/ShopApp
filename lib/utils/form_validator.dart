37a9ebec3d982dfd161d63e383576ee42606b0da
Alin Babu <alinbabu2010@gmail.com>
2023-01-17 21:34:31 +0530

    Add validator class for form validation

---
+++ lib/utils/form_validator.dart
@@ -0,0 +1,42 @@
+class FormValidator {
+  static checkTitle(String? value) {
+    if (value?.isEmpty == true) {
+      return "Please provide a title.";
+    }
+    return null;
+  }
+
+  static String? checkDescription(String? value) {
+    if (value?.isEmpty == true) {
+      return "Please enter a description.";
+    }
+    if (value?.length.compareTo(10).isNegative == true) {
+      return 'Should be at least 10 characters long.';
+    }
+    return null;
+  }
+
+  static String? checkPrice(String? value) {
+    if (value?.isEmpty == true) {
+      return "Please provide a price.";
+    }
+    if (double.tryParse(value!) == null) {
+      return "Please provide a valid number.";
+    }
+    if (double.parse(value) <= 0) {
+      return "Please enter a number greater than zero.";
+    }
+    return null;
+  }
+
+  static String? checkUrl(String? value, {bool checkEmpty = true}) {
+    if (value?.isEmpty == true) {
+      return checkEmpty ? "Please enter an image URL" : null;
+    }
+    if (value?.startsWith('http') == false ||
+        value?.startsWith('https') == false) {
+      return 'Please enter a valid URL';
+    }
+    return null;
+  }
+}
