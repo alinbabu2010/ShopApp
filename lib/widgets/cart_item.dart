538249c6332b4b8bcf24293765b120f782b009cd
Alin Babu <alinbabu2010@gmail.com>
2023-01-17 15:36:10 +0530

    Fix decimal points for total price

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -49,7 +49,9 @@ class CartItem extends StatelessWidget {
               ),
             ),
             title: Text(title),
-            subtitle: Text("Total : \$${price * quantity}"),
+            subtitle: Text(
+              "Total : \$${(price * quantity).toStringAsFixed(2)}",
+            ),
             trailing: Text('$quantity x'),
           ),
         ),
