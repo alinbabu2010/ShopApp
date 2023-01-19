b747cd995100454ca78cd9a5fa3032bd4190056f
Alin Babu <alinbabu2010@gmail.com>
2023-01-19 17:03:05 +0530

    Add function to save order to network database

--- lib/network/network_manager.dart
+++ lib/network/network_manager.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:shop_app/models/product.dart';
 
+import '../models/order_item.dart';
 import '../utils/constants.dart';
 import '../utils/extension.dart';
 
@@ -44,6 +45,11 @@ class NetworkManager {
   static Future<Response> deleteProduct(String productId) async {
     final uri = _createUrl("/products/$productId.json");
     return await delete(uri);
+  }
+
+  static Future<Response> addOrders(OrderItem orderItem) async {
+    final uri = _createUrl("/orders.json");
+    return await post(uri, body: jsonEncode(orderItem));
   }
 
   static Uri _createUrl(String path) {
