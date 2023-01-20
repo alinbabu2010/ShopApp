e9ef6b4086ce8e3d679f6bc7dabf93d10df2f6db
Alin Babu <alinbabu2010@gmail.com>
2023-01-20 14:10:53 +0530

    Add use of the `FutureBuilder`widget to fetch and handle orders

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -7,36 +7,10 @@ import 'package:shop_app/widgets/orders_item.dart';
 
 import '../widgets/empty_msg.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = "/orders_screen";
 
   const OrdersScreen({Key? key}) : super(key: key);
-
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  late Orders orderData;
-  var _isInit = true;
-  var _isLoading = true;
-
-  void _setLoader(bool isLoading) {
-    setState(() {
-      _isLoading = isLoading;
-    });
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (_isInit) {
-      _setLoader(true);
-      orderData = Provider.of<Orders>(context);
-      orderData.fetchOrders().then((_) => _setLoader(false));
-      _isInit = false;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,18 +18,30 @@ class _OrdersScreenState extends State<OrdersScreen> {
       appBar: AppBar(
         title: const Text(constants.yourOrders),
       ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: () => orderData.fetchOrders(),
-              child: orderData.orders.isEmpty
-                  ? const EmptyMsgWidget(message: constants.emptyOrders)
-                  : ListView.builder(
-                      itemBuilder: (_, index) =>
-                          OrdersItem(orderData.orders[index]),
-                      itemCount: orderData.orders.length,
-                    ),
-            ),
+      body: FutureBuilder(
+          future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+          builder: (context, dataSnapShot) {
+            if (dataSnapShot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              if (dataSnapShot.hasError) {
+                return EmptyMsgWidget(message: dataSnapShot.error.toString());
+              } else {
+                return Consumer<Orders>(
+                  builder: (context, orderData, child) => RefreshIndicator(
+                    onRefresh: () => orderData.fetchOrders(),
+                    child: orderData.orders.isEmpty
+                        ? const EmptyMsgWidget(message: constants.emptyOrders)
+                        : ListView.builder(
+                            itemBuilder: (_, index) =>
+                                OrdersItem(orderData.orders[index]),
+                            itemCount: orderData.orders.length,
+                          ),
+                  ),
+                );
+              }
+            }
+          }),
       drawer: const AppDrawer(),
     );
   }
