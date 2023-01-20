251972ccaa1e38e10387a42c88f7cc52238d9f85
Alin Babu <alinbabu2010@gmail.com>
2023-01-20 12:13:53 +0530

    Add exception handling to function parseOrderItem

--- lib/models/parsers/order_item_parser.dart
+++ lib/models/parsers/order_item_parser.dart
@@ -17,14 +17,18 @@ class OrderItemParser {
   }
 
   List<OrderItem> parseOrderItem(Response response) {
-    final extractedData =
-        jsonDecode(response.body.toString()) as Map<String, dynamic>;
-    final List<OrderItem> loadedOrders = [];
-    extractedData.forEach((orderId, data) {
-      final orderItem = OrderItem.fromJson(data as Map<String, dynamic>);
-      orderItem.id = orderId;
-      loadedOrders.add(orderItem);
-    });
-    return loadedOrders;
+    try {
+      final extractedData =
+          jsonDecode(response.body.toString()) as Map<String, dynamic>;
+      final List<OrderItem> loadedOrders = [];
+      extractedData.forEach((orderId, data) {
+        final orderItem = OrderItem.fromJson(data as Map<String, dynamic>);
+        orderItem.id = orderId;
+        loadedOrders.add(orderItem);
+      });
+      return loadedOrders;
+    } catch (error) {
+      return List.empty();
+    }
   }
 }
