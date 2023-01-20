f170d67602975248b55f27af68db0fec705cb6ca
Alin Babu <alinbabu2010@gmail.com>
2023-01-20 12:15:45 +0530

    Add functionality to fetch orders and handle loader and empty orders case in orders screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,22 +5,63 @@ import 'package:shop_app/utils/constants.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/orders_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = "/orders_screen";
 
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  late Orders orderData;
+  var _isInit = true;
+  var _isLoading = true;
+
+  void _setLoader(bool isLoading) {
+    setState(() {
+      _isLoading = isLoading;
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_isInit) {
+      _setLoader(true);
+      orderData = Provider.of<Orders>(context);
+      orderData.fetchOrders().then((_) => _setLoader(false));
+      _isInit = false;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text(yourOrders),
       ),
-      body: ListView.builder(
-        itemBuilder: (_, index) => OrdersItem(orderData.orders[index]),
-        itemCount: orderData.orders.length,
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : RefreshIndicator(
+              onRefresh: () => orderData.fetchOrders(),
+              child: orderData.orders.isEmpty
+                  ? SingleChildScrollView(
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      child: SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.8,
+                        child: const Center(
+                          child: Text("No orders to show."),
+                        ),
+                      ),
+                    )
+                  : ListView.builder(
+                      itemBuilder: (_, index) =>
+                          OrdersItem(orderData.orders[index]),
+                      itemCount: orderData.orders.length,
+                    ),
+            ),
       drawer: const AppDrawer(),
     );
   }
