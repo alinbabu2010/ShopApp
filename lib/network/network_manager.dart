9fedf6d0753bc83238376577f40488c4ae0201ea
Alin Babu <alinbabu2010@gmail.com>
2023-01-23 18:43:09 +0530

    Make network manager to singleton class

--- lib/network/network_manager.dart
+++ lib/network/network_manager.dart
@@ -10,13 +10,21 @@ import '../models/parsers/order_item_parser.dart';
 import '../utils/constants.dart' as constants;
 
 class NetworkManager {
-  static Future<String> addProduct(Product product) {
+  NetworkManager();
+
+  NetworkManager? networkManager;
+
+  NetworkManager.newInstance() {
+    networkManager ??= NetworkManager();
+  }
+
+  Future<String> addProduct(Product product) {
     var uri = _createUrl("/products.json");
     return post(uri, body: jsonEncode(product))
         .then((response) => jsonDecode(response.body)['name']);
   }
 
-  static Future<List<Product>> fetchProducts() async {
+  Future<List<Product>> fetchProducts() async {
     var uri = _createUrl("/products.json");
     try {
       final response = await get(uri);
@@ -30,17 +38,17 @@ class NetworkManager {
     }
   }
 
-  static Future<Response> updateProduct(String productId, Product product) {
+  Future<Response> updateProduct(String productId, Product product) {
     final uri = _createUrl("/products/$productId.json");
     return patch(uri, body: jsonEncode(product));
   }
 
-  static Future<Response> deleteProduct(String productId) async {
+  Future<Response> deleteProduct(String productId) async {
     final uri = _createUrl("/products/$productId.json");
     return await delete(uri);
   }
 
-  static Future<OrderItem> addOrders(OrderItem orderItem) async {
+  Future<OrderItem> addOrders(OrderItem orderItem) async {
     final uri = _createUrl("/orders.json");
     try {
       var response = await post(uri, body: jsonEncode(orderItem));
@@ -55,7 +63,7 @@ class NetworkManager {
     }
   }
 
-  static Future<List<OrderItem>> fetchOrders() async {
+  Future<List<OrderItem>> fetchOrders() async {
     final uri = _createUrl("/orders.json");
     try {
       var response = await get(uri);
@@ -69,7 +77,7 @@ class NetworkManager {
     }
   }
 
-  static Uri _createUrl(String path) {
+  Uri _createUrl(String path) {
     return Uri.https(constants.baseUrl, path);
   }
 }
