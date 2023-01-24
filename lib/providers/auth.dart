bd770be26f539b08ec02a752515d32538e25cfb6
Alin Babu <alinbabu2010@gmail.com>
2023-01-24 10:12:24 +0530

    Make auth provider to store token locally and handle success response
    Add function to set data in auth provider properties

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/widgets.dart';
+import 'package:shop_app/models/auth_success_response.dart';
 import 'package:shop_app/models/signup_request.dart';
 import 'package:shop_app/network/auth_manager.dart';
 
 class Auth with ChangeNotifier {
-  late String _token;
-  late DateTime _expiryDate;
-  late String _userId;
+  String? _token;
+  DateTime? _expiryDate;
+  String? _userId;
 
   final authManager = AuthManager.newInstance();
 
+  bool get isAuth => token != null;
+
+  String? get token {
+    if (_expiryDate != null &&
+        _expiryDate?.isAfter(DateTime.now()) == true &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
+  void _setData(AuthSuccessResponse response) {
+    _token = response.idToken;
+    _expiryDate = DateTime.now().add(
+      Duration(seconds: int.parse(response.expiresIn.toString())),
+    );
+    _userId = response.localId;
+    notifyListeners();
+  }
+
   Future<void> signup(String email, String password) async {
     final requestModel = SignupRequest(email, password);
-    return authManager.signup(requestModel);
+    return authManager
+        .signup(requestModel)
+        .then((response) => _setData(response));
   }
 
   Future<void> signIn(String email, String password) async {
     final requestModel = SignupRequest(email, password);
-    return authManager.signIn(requestModel);
+    return authManager
+        .signIn(requestModel)
+        .then((response) => _setData(response));
   }
 }
