df6c3e5f99237c83c05c50160c6b51e348eec8cb
Alin Babu <alinbabu2010@gmail.com>
2023-01-25 18:29:41 +0530

    Add manager class for shared preferences

---
+++ lib/models/data/preference_manager.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shop_app/models/auth_success_response.dart';
+
+class PreferenceManager {
+  SharedPreferences? prefs;
+
+  static const prefKey = "authCredentials";
+
+  Future<SharedPreferences> setupPreference() {
+    return SharedPreferences.getInstance();
+  }
+
+  void saveUserData(AuthSuccessResponse response) {
+    final authCredentials = jsonEncode(response);
+    prefs?.setString(prefKey, authCredentials);
+  }
+
+  AuthSuccessResponse? getUserData(SharedPreferences prefs) {
+    this.prefs = prefs;
+    if (prefs.containsKey(prefKey) == false) return null;
+    try {
+      final prefData = jsonDecode(prefs.getString(prefKey).toString());
+      return AuthSuccessResponse.fromJson(prefData);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  Future<bool>? clearUserData() {
+    return prefs?.clear();
+  }
+}
