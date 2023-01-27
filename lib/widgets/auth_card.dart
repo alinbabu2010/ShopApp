8af1785dd2b8bb3ce69a8eb74a02c2c4aa2e39f9
Alin Babu <alinbabu2010@gmail.com>
2023-01-27 15:46:07 +0530

    Add fade and slide transition widget to animate confirm password field

--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -31,7 +31,8 @@ class _AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
   late AnimationController _controller;
-  late Animation<Size> _heightAnimation;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _opacityAnimation;
 
   @override
   void initState() {
@@ -40,21 +41,19 @@ class _AuthCardState extends State<AuthCard>
       vsync: this,
       duration: const Duration(milliseconds: 300),
     );
-    _heightAnimation = Tween<Size>(
-      begin: const Size(
-        double.infinity,
-        dimens.authCardLoginHeight,
-      ),
-      end: const Size(
-        double.infinity,
-        dimens.authCardSignupHeight,
-      ),
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -1.5),
+      end: const Offset(0, 0),
     ).animate(
       CurvedAnimation(
         parent: _controller,
         curve: Curves.linear,
       ),
     );
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    ));
     //_heightAnimation.addListener(() => setState(() {}));
   }
 
@@ -118,8 +117,10 @@ class _AuthCardState extends State<AuthCard>
     setState(() {
       if (_authMode == AuthMode.login) {
         _authMode = AuthMode.signup;
+        _controller.forward();
       } else {
         _authMode = AuthMode.login;
+        _controller.reverse();
       }
     });
   }
@@ -189,23 +190,36 @@ class _AuthCardState extends State<AuthCard>
                     _authData[constants.passwordKey] = value!;
                   },
                 ),
-                if (_authMode == AuthMode.signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.signup,
-                    decoration: const InputDecoration(
-                      labelText: constants.labelConfirmPassword,
-                    ),
-                    obscureText: true,
-                    textInputAction: TextInputAction.done,
-                    validator: _authMode == AuthMode.signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return constants.passwordMatchError;
-                            }
-                            return null;
-                          }
-                        : null,
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.signup ? 120 : 0,
                   ),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.signup,
+                        decoration: const InputDecoration(
+                          labelText: constants.labelConfirmPassword,
+                        ),
+                        obscureText: true,
+                        textInputAction: TextInputAction.done,
+                        validator: _authMode == AuthMode.signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return constants.passwordMatchError;
+                                }
+                                return null;
+                              }
+                            : null,
+                      ),
+                    ),
+                  ),
+                ),
                 const SizedBox(
                   height: dimens.authCardSizedBoxHeight,
                 ),
