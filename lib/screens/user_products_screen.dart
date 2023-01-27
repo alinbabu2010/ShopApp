44dcab828a0cef9beae93951c81bf89fa115e1db
Alin Babu <alinbabu2010@gmail.com>
2023-01-27 11:08:08 +0530

    Make screen to stateful widget and add `FutureProvider` to load products
    Also add method and property to handle data refresh. Fix issues in data refresh in user_products_screen.dart

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -6,17 +6,41 @@ import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/user_product_item.dart';
 
 import '../providers/products.dart';
+import '../widgets/empty_msg.dart';
 
-class UserProductsScreen extends StatelessWidget {
+class UserProductsScreen extends StatefulWidget {
   static const routeName = "/user_product_screen";
 
   const UserProductsScreen({Key? key}) : super(key: key);
 
-  Future<void> _refreshProducts(BuildContext context) async {
+  @override
+  State<UserProductsScreen> createState() => _UserProductsScreenState();
+}
+
+class _UserProductsScreenState extends State<UserProductsScreen> {
+  late Future _productsFuture;
+  var _isRefresh = false;
+
+  Future<void> _obtainProductsFuture() async {
     await Provider.of<Products>(
       context,
       listen: false,
-    ).fetchAndSetProducts();
+    ).fetchAndSetProducts().catchError((error) => Future.error(error));
+  }
+
+  @override
+  void initState() {
+    _productsFuture = _obtainProductsFuture();
+    super.initState();
+  }
+
+  Future<void> _refreshProducts() async {
+    final refreshFuture =
+        _obtainProductsFuture().whenComplete(() => _isRefresh = false);
+    _isRefresh = true;
+    setState(() {
+      _productsFuture = refreshFuture;
+    });
   }
 
   @override
@@ -34,24 +58,36 @@ class UserProductsScreen extends StatelessWidget {
           )
         ],
       ),
-      body: RefreshIndicator(
-        onRefresh: () => _refreshProducts(context),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: ListView.builder(
-            itemBuilder: (_, index) {
-              var product = productsData.items[index];
-              return Column(
-                children: [
-                  UserProductItem(product.id, product.title, product.imageUrl),
-                  const Divider(color: Colors.black45),
-                ],
+      body: FutureBuilder(
+          future: _productsFuture,
+          builder: (_, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting &&
+                !_isRefresh) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return RefreshIndicator(
+                onRefresh: _refreshProducts,
+                child: dataSnapshot.hasError
+                    ? EmptyMsgWidget(message: dataSnapshot.error.toString())
+                    : Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: ListView.builder(
+                          itemBuilder: (_, index) {
+                            var product = productsData.items[index];
+                            return Column(
+                              children: [
+                                UserProductItem(product.id, product.title,
+                                    product.imageUrl),
+                                const Divider(color: Colors.black45),
+                              ],
+                            );
+                          },
+                          itemCount: productsData.items.length,
+                        ),
+                      ),
               );
-            },
-            itemCount: productsData.items.length,
-          ),
-        ),
-      ),
+            }
+          }),
       drawer: const AppDrawer(),
     );
   }
