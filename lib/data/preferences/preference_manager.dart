2f64caf3f14c2fab3e3fa41522a8cf6ef6429d70
Alin Babu <alinbabu2010@gmail.com>
2023-03-02 16:47:53 +0530

    Add function to save and get expiry date in preference

--- lib/data/preferences/preference_manager.dart
+++ lib/data/preferences/preference_manager.dart
@@ -8,6 +8,7 @@ class PreferenceManager {
   SharedPreferences? prefs;
 
   static const prefKey = "authCredentials";
+  static const prefExpireKey = "expirykey";
 
   Future<SharedPreferences> setupPreference() {
     return SharedPreferences.getInstance();
@@ -16,6 +17,23 @@ class PreferenceManager {
   void saveUserData(AuthSuccessResponse response) {
     final authCredentials = jsonEncode(response);
     prefs?.setString(prefKey, authCredentials);
+  }
+
+  void saveExpiryDate(String? expiresIn) {
+    DateTime dateTime = DateTime.now().add(Duration(
+      seconds: int.parse(expiresIn ?? "0"),
+    ));
+    prefs?.setString(prefExpireKey, dateTime.toIso8601String());
+  }
+
+  DateTime? getExpiryTime() {
+    if (prefs?.containsKey(prefExpireKey) == false) return null;
+    try {
+      final prefData = prefs?.getString(prefExpireKey);
+      return DateTime.parse(prefData ?? "");
+    } catch (error) {
+      return null;
+    }
   }
 
   AuthSuccessResponse? getUserData(SharedPreferences prefs) {
